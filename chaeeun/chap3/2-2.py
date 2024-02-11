9a33147fcf72fa115634cc3b63a2bfa84bf4b6b0
rachaen <rachaenlee@gmail.com>
2024-02-12 00:08:29 +0900

    chap3-2 제어문

---
+++ chaeeun/chap3/2-2.py
@@ -0,0 +1,11 @@
+age = 15
+if age < 8 :
+  print('유치원생')
+elif age < 14 :
+  print('초등학생')
+elif age < 17 :
+  print('중학생')
+elif age < 20 :
+  print('고등학생')
+else :
+  print('대학생')
---
+++ chaeeun/chap3/2-5.py
@@ -0,0 +1,4 @@
+d = {'class' : 1, 'number' : 5, 'name' : 'minho'}
+
+for key in d.keys() :
+  print(key)
